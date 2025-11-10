c44767dc5e373450bea75e95f1050c7e6718844b
slec16 <sankopytov.76@mail.ru>
2025-11-11 02:22:14 +0300

    feat: api client

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
+import { Provider } from 'react-redux'
+import { store } from './store/index.ts'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
