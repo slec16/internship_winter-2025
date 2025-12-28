3051064c4399273af29ab925d4918bf6da59b192
slec16 <sankopytov.76@mail.ru>
2025-12-28 23:29:44 +0300

    feat: RHF for common form

--- src/features/common-form/ui/CommonForm.tsx
+++ src/features/common-form/ui/CommonForm.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react"
 import {
     Box,
     Button,
     ButtonGroup,
     Field,
+    Text,
     Fieldset,
     For,
     Input,
@@ -17,13 +17,13 @@ import type { Item } from "@/shared/types/items"
 interface commonFormProps {
     itemData: Item | null,
     stepsStore: UseStepsReturn
-    onChange: React.Dispatch<React.SetStateAction<{}>>
+    onChange: React.Dispatch<React.SetStateAction<Item | null>>
 }
 
 interface IFormInput {
     name: string,
     description: string,
-    locaion: string,
+    location: string,
     image: string,
     type: string
 }
@@ -32,120 +32,147 @@ const CommonForm = (props: commonFormProps) => {
 
     const { itemData, stepsStore, onChange } = props
 
-    const { control, handleSubmit } = useForm({
+    const { control, handleSubmit,  formState: { errors } } = useForm({
         defaultValues: {
             name: "",
             description: "",
-            locaion: "",
+            location: "",
             image: "",
             type: ""
         },
     })
 
     const onSubmit: SubmitHandler<IFormInput> = (data) => {
-        console.log(data)
-    }
-
-    
-
-    const [name, setName] = useState("")
-    const [description, setDescription] = useState("")
-    const [location, setLocation] = useState("")
-    const [image, setImage] = useState("")
-    const [type, setType] = useState("")
-
-    const nextStep = () => {
-        console.log('next')
-        // handleSubmit(onSubmit)
-        // const base = {
-        //     name: name,
-        //     description: description,
-        //     location: location,
-        //     image: image,
-        //     type: type
-        // }
-        // onChange(base)
-        // stepsStore.goToNextStep()
+        stepsStore.goToNextStep()
+        // TODO: use here baseItem type
+        onChange(data)
     }
 
     return (
         <Box>
             <form onSubmit={handleSubmit(onSubmit)}>
-            <Fieldset.Root size="lg" maxW="md" mb="10">
-                <Stack>
-                    <Fieldset.Legend color="text">Данные объявления</Fieldset.Legend>
-                    <Fieldset.HelperText color="secondaryText">
-                        Пожалуйста укажите следующую инфорамцию
-                    </Fieldset.HelperText>
-                </Stack>
+                <Fieldset.Root size="lg" maxW="md" mb="10">
+                    <Stack>
+                        <Fieldset.Legend color="text">Данные объявления</Fieldset.Legend>
+                        <Fieldset.HelperText color="secondaryText">
+                            Пожалуйста укажите следующую инфорамцию
+                        </Fieldset.HelperText>
+                    </Stack>
 
-                <Fieldset.Content>
-                    <Field.Root>
-                        <Field.Label>Название</Field.Label>
-                        <Controller
-                            name="name"
-                            control={control}
-                            render={({ field }) => <Input {...field} />}
-                        />
-                        {/* <Input
-                            name="title"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        /> */}
-                    </Field.Root>
-                    <Field.Root>
-                        <Field.Label>Описание</Field.Label>
-                        <Input
-                            name="description"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
-                    </Field.Root>
-                    <Field.Root>
-                        <Field.Label>Локация</Field.Label>
-                        <Input
-                            name="location"
-                            value={location}
-                            onChange={(e) => setLocation(e.target.value)}
-                        />
-                    </Field.Root>
-                    <Field.Root>
-                        <Field.Label>Фото</Field.Label>
-                        <Input
-                            name="image"
-                            value={image}
-                            onChange={(e) => setImage(e.target.value)}
-                        />
-                    </Field.Root>
+                    <Fieldset.Content>
+                        <Field.Root>
+                            <Field.Label>Название</Field.Label>
+                            <Controller
+                                name="name"
+                                control={control}
+                                rules={{
+                                    required: "Это поле обязательно для заполнения" 
+                                }}
+                                render={({ field, fieldState }) => (
+                                    <Box w="full">
+                                        <Input {...field} borderColor={fieldState.error ? "red.500" : "gray.200"} outlineColor={fieldState.error ? "red.500" : "gray.200"}/>
+                                        {fieldState.error && (
+                                            <Text color="red">
+                                                {fieldState.error.message}
+                                            </Text>
+                                        )}
+                                    </Box>
+                                )}
+                            />
+                        </Field.Root>
+                        <Field.Root>
+                            <Field.Label>Описание</Field.Label>
+                            <Controller
+                                name="description"
+                                control={control}
+                                rules={{
+                                    required: "Это поле обязательно для заполнения" 
+                                }}
+                                render={({ field, fieldState }) => (
+                                    <Box w="full">
+                                        <Input {...field} borderColor={fieldState.error ? "red.500" : "gray.200"} outlineColor={fieldState.error ? "red.500" : "gray.200"}/>
+                                        {fieldState.error && (
+                                            <Text color="red">
+                                                {fieldState.error.message}
+                                            </Text>
+                                        )}
+                                    </Box>
+                                )}
+                            />
+                        </Field.Root>
+                        <Field.Root>
+                            <Field.Label>Локация</Field.Label>
+                            <Controller
+                                name="location"
+                                control={control}
+                                rules={{
+                                    required: "Это поле обязательно для заполнения" 
+                                }}
+                                render={({ field, fieldState }) => (
+                                    <Box w="full">
+                                        <Input {...field} borderColor={fieldState.error ? "red.500" : "gray.200"} outlineColor={fieldState.error ? "red.500" : "gray.200"}/>
+                                        {fieldState.error && (
+                                            <Text color="red">
+                                                {fieldState.error.message}
+                                            </Text>
+                                        )}
+                                    </Box>
+                                )}
+                            />
+                        </Field.Root>
+                        <Field.Root>
+                            <Field.Label>Фото</Field.Label>
+                            <Controller
+                                name="image"
+                                control={control}
+                                render={({ field }) => <Input {...field} />}
+                            />
+                        </Field.Root>
 
-                    {/* TODO: default value */}
-                    <Field.Root>
-                        <Field.Label>Тип объявления</Field.Label>
-                        <NativeSelect.Root>
-                            <NativeSelect.Field
-                                name="тип"
-                                value={type}
-                                onChange={(e) => setType(e.target.value)}
-                            >
-                                <For each={["Авто", "Недвижимость", "Услуги"]}>
-                                    {(item) => (
-                                        <option key={item} value={item}>
-                                            {item}
-                                        </option>
-                                    )}
-                                </For>
-                            </NativeSelect.Field>
-                            <NativeSelect.Indicator />
-                        </NativeSelect.Root>
-                    </Field.Root>
-                </Fieldset.Content>
-            </Fieldset.Root>
+                        <Field.Root>
+                            <Field.Label>Тип объявления</Field.Label>
+                            <Controller
+                                name="type"
+                                control={control}
+                                rules={{
+                                    required: "Это поле обязательно для заполнения"
+                                }}
+                                render={({ field, fieldState }) => (
+                                    <Box w="full">
+                                        <NativeSelect.Root >
+                                            <NativeSelect.Field
+                                                {...field}
+                                                placeholder="Выберете тип"
+                                                borderColor={fieldState.error ? "red.500" : "gray.200"}
+                                                outlineColor={fieldState.error ? "red.500" : "gray.200"}
+                                            >
+                                                <For each={["Авто", "Недвижимость", "Услуги"]}>
+                                                    {(item) => (
+                                                        <option key={item} value={item}>
+                                                            {item}
+                                                        </option>
+                                                    )}
+                                                </For>
+                                            </NativeSelect.Field>
+                                            <NativeSelect.Indicator />
+                                        </NativeSelect.Root>
+                                        {fieldState.error && (
+                                            <Text color="red">
+                                                {fieldState.error.message}
+                                            </Text>
+                                        )}
+                                    </Box>
+                                )}
+                            />
+                        </Field.Root>
+                    </Fieldset.Content>
+                </Fieldset.Root>
 
-           
-            <ButtonGroup size="sm" variant="outline">
-                <Button bg="buttonPrimary" disabled={!stepsStore.hasPrevStep}>Назад</Button>
-                <Button type="submit"  bg="buttonPrimary" onClick={nextStep}>Далее</Button>
-            </ButtonGroup>
+
+                <ButtonGroup size="sm" variant="outline">
+                    <Button bg="buttonPrimary" disabled={!stepsStore.hasPrevStep}>Назад</Button>
+                    <Button type="submit" bg="buttonPrimary">Далее</Button>
+                </ButtonGroup>
             </form>
         </Box>
     )
